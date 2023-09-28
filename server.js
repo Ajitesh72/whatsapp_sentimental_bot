dc648ac53e2ad93d62ee95a51e957d91a296fda6
Ajitesh <Ajitesh72>
2023-09-29 00:07:36 +0530

    sentiment score added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { create, Client } = require("@open-wa/wa-automate");
 const { spawn } = require("child_process");
 
-// Create a WhatsApp bot using wa-automate
+// WhatsApp bot using wa-automate
 create()
   .then((client) => {
     console.log("WhatsApp bot is now connected.");
@@ -9,7 +9,7 @@ create()
     client.onMessage(async (message) => {
       console.log(`Received message from ${message.from}: ${message.body}`);
 
-      // Send the message content to the Python NLP script for processing
+      // sending to Python script
       const message_extracted = message.body;
       console.log("mssg is", message_extracted);
 
@@ -21,13 +21,7 @@ create()
       pythonProcess.stdout.on("data", (data) => {
         try {
           const outputText_sentiment = data.toString("utf8");
-          console.log("Data is:", outputText_sentiment); // Process the extracted information from Python
-
-
-          // For demonstration purposes, let's send a response back to the user
-          // const responseMessage = `Extracted Date: ${extractedInfo.dates.join(
-          //   ", "
-          // )}\nSentiment: ${outputText_sentiment}`;
+          console.log("Data is:", outputText_sentiment); 
           if (outputText_sentiment === positive) {
             client.sendText("thanks for your positive message");
           }
--- whatsapp_nlp.py
+++ whatsapp_nlp.py
@@ -2,13 +2,11 @@ import sys
 from textblob import TextBlob
 
 def perform_sentiment_analysis(text):
-    # Create a TextBlob object from the message content
     blob = TextBlob(text)
 
-    # Perform sentiment analysis
+    # Perform sentiment analysis(polarity score)
     sentiment_score = blob.sentiment.polarity
 
-    # Determine sentiment based on the polarity score
     if sentiment_score > 0:
         sentiment = "Positive"
     elif sentiment_score < 0:
@@ -25,5 +23,4 @@ if __name__ == "__main__":
     # Perform sentiment analysis
     sentiment = perform_sentiment_analysis(message_content)
 
-    # Print the sentiment
     print(sentiment)
